651a00c89886ed3fc915552706bd70d9eddcc034
jegorix <0311egor@gmail.com>
2025-02-22 02:49:38 +0300

    first form create

--- curproject/boiler/views.py
+++ curproject/boiler/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from .forms import UserForm
 from django.http import HttpResponse
 # Create your views here.
 
 
 def home(request):
+    userform = UserForm()
     data = {
-        'age': 5,
+        'age': 19,
+        'form': userform,
     }
-    return render(request, 'boiler/index.html', data)
+    head_items = [
+        'info', 'about', 'goods', 'contact',
+    ]
+    return render(request, 'boiler/index.html', context= {'head_items': head_items, 'data':data})
 
 def about(request):
     # return render(requset, '<h1>Hi message!</h1>')
