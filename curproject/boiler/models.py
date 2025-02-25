f8a4116ea85945c9a2095ff73b2874628380acac
jegorix <0311egor@gmail.com>
2025-02-26 01:31:23 +0300

    create migrations

--- curproject/boiler/models.py
+++ curproject/boiler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Boiler(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+    sex = models.CharField(max_length=10, default='male')
+    content = models.TextField(max_length=500)
