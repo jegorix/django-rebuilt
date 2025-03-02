a05984fa76963bae96a839b43377e91f8942c1f0
jegorix <0311egor@gmail.com>
2025-03-02 10:57:06 +0300

    add boolean and nullboolean fields

--- curproject/boiler/forms.py
+++ curproject/boiler/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField(label = 'First Name')
-    age = forms.IntegerField(label = 'Full age')
-    sex = forms.CharField(label = 'Gender')
-    text = forms.CharField(label = 'Content')
+    vyb = forms.NullBooleanField(label = "Do you want to leave this page?")
+    basket = forms.BooleanField(label = "Do you want to buy this good?", required = False)
+#     name = forms.CharField(label = 'First Name', initial = 'your name...')
+#     age = forms.IntegerField(label = 'Full age')
+#     sex = forms.CharField(label = 'Gender')
+#     text = forms.CharField(label = 'Content', help_text = 'No more 100 symbols')
+# f = UserForm(auto_id = False)
