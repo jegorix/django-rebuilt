2338bafa1b217afb6c845a80a0fb4907cb6b87e0
jegorix <0311egor@gmail.com>
2025-03-02 11:11:15 +0300

    add email field, ip_adress field

--- curproject/boiler/forms.py
+++ curproject/boiler/forms.py
@@ -3,6 +3,10 @@ from django import forms
 class UserForm(forms.Form):
     vyb = forms.NullBooleanField(label = "Do you want to leave this page?")
     basket = forms.BooleanField(label = "Do you want to buy this good?", required = False)
+    name = forms.CharField(label = "username", required = False)
+    email = forms.EmailField(label = "email", required = False)
+    ip_adress = forms.GenericIPAddressField(label = "IP adress", help_text = "Example: 192.168.127.12")
+
 #     name = forms.CharField(label = 'First Name', initial = 'your name...')
 #     age = forms.IntegerField(label = 'Full age')
 #     sex = forms.CharField(label = 'Gender')
